eb731f64fc558c1dfe7979cde54b7b4cfde4eb5d
wbecker <wbecker@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-12-10 16:23:41 +0000

    Ver 1.5.1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/macina_banners/trunk@27576 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "macina_banners"
 #
-# Auto generated 04-11-2009 17:53
+# Auto generated 10-12-2009 17:10
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Bannermanagement Tool with Bannerplacement on Frontend, Bannerrotation, sheduling, and Backendstatistics. Modified by media lights gmbh (Markus Friedrich)',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '1.5.0',
+	'version' => '1.5.1',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -42,7 +42,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:36:{s:64:"class.tx_macinabanners_tt_content_tx_macinabanners_placement.php";s:4:"5b0e";s:21:"ext_conf_template.txt";s:4:"8c7c";s:12:"ext_icon.gif";s:4:"3efe";s:17:"ext_localconf.php";s:4:"8e54";s:14:"ext_tables.php";s:4:"4f8f";s:14:"ext_tables.sql";s:4:"08ec";s:24:"ext_typoscript_setup.txt";s:4:"be67";s:33:"icon_tx_macinabanners_banners.gif";s:4:"ed12";s:36:"icon_tx_macinabanners_banners__x.gif";s:4:"fa41";s:13:"locallang.php";s:4:"d129";s:25:"locallang_csh_banners.php";s:4:"7634";s:16:"locallang_db.php";s:4:"6ae2";s:46:"selicon_tt_content_tx_macinabanners_mode_0.gif";s:4:"9cd1";s:46:"selicon_tt_content_tx_macinabanners_mode_1.gif";s:4:"82d9";s:46:"selicon_tt_content_tx_macinabanners_mode_2.gif";s:4:"a38b";s:51:"selicon_tt_content_tx_macinabanners_placement_0.gif";s:4:"d574";s:51:"selicon_tt_content_tx_macinabanners_placement_1.gif";s:4:"bf98";s:51:"selicon_tt_content_tx_macinabanners_placement_2.gif";s:4:"5a13";s:51:"selicon_tt_content_tx_macinabanners_placement_3.gif";s:4:"824b";s:48:"selicon_tx_macinabanners_banners_placement_0.gif";s:4:"d574";s:48:"selicon_tx_macinabanners_banners_placement_1.gif";s:4:"bf98";s:48:"selicon_tx_macinabanners_banners_placement_2.gif";s:4:"5a13";s:48:"selicon_tx_macinabanners_banners_placement_3.gif";s:4:"824b";s:7:"tca.php";s:4:"0f4e";s:14:"pi1/ce_wiz.gif";s:4:"3efe";s:34:"pi1/class.tx_macinabanners_pi1.php";s:4:"9b2a";s:42:"pi1/class.tx_macinabanners_pi1_wizicon.php";s:4:"f6cf";s:13:"pi1/clear.gif";s:4:"cc11";s:19:"pi1/flashbanner.fla";s:4:"5a8e";s:19:"pi1/flashbanner.swf";s:4:"aed7";s:17:"pi1/locallang.php";s:4:"c94b";s:16:"pi1/template.htm";s:4:"339b";s:24:"pi1/static/editorcfg.txt";s:4:"527f";s:20:"pi1/static/setup.txt";s:4:"22f7";s:14:"doc/manual.sxw";s:4:"96e5";s:18:"doc/screenshot.gif";s:4:"2ee1";}',
+	'_md5_values_when_last_written' => 'a:36:{s:64:"class.tx_macinabanners_tt_content_tx_macinabanners_placement.php";s:4:"5b0e";s:21:"ext_conf_template.txt";s:4:"8c7c";s:12:"ext_icon.gif";s:4:"3efe";s:17:"ext_localconf.php";s:4:"8e54";s:14:"ext_tables.php";s:4:"4f8f";s:14:"ext_tables.sql";s:4:"08ec";s:24:"ext_typoscript_setup.txt";s:4:"be67";s:33:"icon_tx_macinabanners_banners.gif";s:4:"ed12";s:36:"icon_tx_macinabanners_banners__x.gif";s:4:"fa41";s:13:"locallang.php";s:4:"d129";s:25:"locallang_csh_banners.php";s:4:"7634";s:16:"locallang_db.php";s:4:"b789";s:46:"selicon_tt_content_tx_macinabanners_mode_0.gif";s:4:"9cd1";s:46:"selicon_tt_content_tx_macinabanners_mode_1.gif";s:4:"82d9";s:46:"selicon_tt_content_tx_macinabanners_mode_2.gif";s:4:"a38b";s:51:"selicon_tt_content_tx_macinabanners_placement_0.gif";s:4:"d574";s:51:"selicon_tt_content_tx_macinabanners_placement_1.gif";s:4:"bf98";s:51:"selicon_tt_content_tx_macinabanners_placement_2.gif";s:4:"5a13";s:51:"selicon_tt_content_tx_macinabanners_placement_3.gif";s:4:"824b";s:48:"selicon_tx_macinabanners_banners_placement_0.gif";s:4:"d574";s:48:"selicon_tx_macinabanners_banners_placement_1.gif";s:4:"bf98";s:48:"selicon_tx_macinabanners_banners_placement_2.gif";s:4:"5a13";s:48:"selicon_tx_macinabanners_banners_placement_3.gif";s:4:"824b";s:7:"tca.php";s:4:"0f4e";s:14:"pi1/ce_wiz.gif";s:4:"3efe";s:34:"pi1/class.tx_macinabanners_pi1.php";s:4:"c1b5";s:42:"pi1/class.tx_macinabanners_pi1_wizicon.php";s:4:"f6cf";s:13:"pi1/clear.gif";s:4:"cc11";s:19:"pi1/flashbanner.fla";s:4:"5a8e";s:19:"pi1/flashbanner.swf";s:4:"aed7";s:17:"pi1/locallang.php";s:4:"c94b";s:16:"pi1/template.htm";s:4:"339b";s:24:"pi1/static/editorcfg.txt";s:4:"527f";s:20:"pi1/static/setup.txt";s:4:"68b3";s:14:"doc/manual.sxw";s:4:"721a";s:18:"doc/screenshot.gif";s:4:"2ee1";}',
+	'suggests' => array(
+	),
 );
 
 ?>
